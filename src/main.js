07a80f5f16029be50af6aeac237b79f94269e676
nishida <k.nishida@vistage-llc.com>
2020-08-05 13:01:56 +0900

    firebase接続設定

--- src/main.js
+++ src/main.js
@@ -1,10 +1,28 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import firebase from "firebase/app";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCiYobfnaRfLHnjrFLFRaryc9FVL6_ODhc",
+    authDomain: "vue-slack-clone-679fe.firebaseapp.com",
+    databaseURL: "https://vue-slack-clone-679fe.firebaseio.com",
+    projectId: "vue-slack-clone-679fe",
+    storageBucket: "vue-slack-clone-679fe.appspot.com",
+    messagingSenderId: "1071991328741",
+    appId: "1:1071991328741:web:7a85eca3fa2393412fe90c",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+window.firebase = firebase;
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    render: (h) => h(App),
+}).$mount("#app");
