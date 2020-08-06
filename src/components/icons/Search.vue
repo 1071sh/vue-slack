a637c67546e6d4c85bc2b4b4be670a34a4aacf76
nishida <k.nishida@vistage-llc.com>
2020-08-06 11:11:22 +0900

    各種アイコン設定

---
+++ src/components/icons/Search.vue
@@ -0,0 +1,17 @@
+<template>
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        class="h-6 w-6 text-gray-700"
+        fill="none"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        stroke="currentColor"
+    >
+        <path
+            class="heroicon-ui"
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+        ></path>
+    </svg>
+</template>
