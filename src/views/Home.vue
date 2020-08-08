e3e0338498fe845924413c661bddc058d9d562c9
nishida <k.nishida@vistage-llc.com>
2020-08-08 10:22:50 +0900

    チャンネルの作成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,7 +11,52 @@
             </div>
             <div class="mt-5 flex justify-between items-center">
                 <div class="font-bold opacity-50 text-lg">チャンネル</div>
-                <PlusCircle />
+                <div
+                    class="z-10 fixed top-0 left-0 h-full w-full flex items-center justify-center"
+                    style="background-color:rgba(0,0,0,0.5)"
+                    v-show="channelModal"
+                    @click="closeChannelModal"
+                >
+                    <div
+                        class="z-20 bg-white text-gray-900 w-1/3 rounded-md"
+                        v-on:click.stop
+                    >
+                        <div class="flex flex-col p-6">
+                            <div class="flex justify-between items-center">
+                                <h2 class="text-3xl font-black leading-loose">
+                                    チャンネルを作成する
+                                </h2>
+                                <span
+                                    @click="closeChannelModal"
+                                    class="text-4xl"
+                                >
+                                    ×
+                                </span>
+                            </div>
+                            <p>
+                                チャンネルはチームがコニュニケーションを取る場所です。特定のトピックに基づいてチャンネルを作ると良いでしょう。(例:
+                                #マーケティング)
+                            </p>
+                            <div class="mt-8 font-semibold">名前</div>
+                            <div class="my-3">
+                                <input
+                                    type="text"
+                                    class="w-full rounded border-gray-900 border-solid border p-3"
+                                    v-model="channel"
+                                />
+                            </div>
+                            <div class="flex justify-end">
+                                <button
+                                    class="px-8 py-2 rounded bg-green-900 font-bold text-white"
+                                    @click="addChannel"
+                                >
+                                    作成
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <PlusCircle @click.native="showChannelModal" />
             </div>
             <div
                 class="opacity-50 mt-1"
@@ -32,7 +77,9 @@
                 :key="user.user_id"
             >
                 <span class="bg-yellow-400 rounded-full w-3 h-3 mr-2"></span>
-                <span class="opacity-50">{{ user.email }}</span>
+                <span class="opacity-50" @click="directMessage(user.email)">
+                    {{ user.email }}
+                </span>
             </div>
         </div>
         <div class="flex-grow bg-gray-100">
@@ -54,7 +101,7 @@
                         <Star class="mx-2" />
                         <button
                             class="py-1 px-4 bg-gray-800 text-white rounded"
-                            @click="singOut"
+                            @click="signOut"
                         >
                             サインアウト
                         </button>
@@ -68,18 +115,20 @@
                             <Avator :user="user.email" />
                             <div class="ml-2">
                                 <div class="font-bold">{{ user.email }}</div>
-                                <div>初めてのメッセージ</div>
+                                <div>{{ message }}</div>
                             </div>
                         </div>
                     </div>
                     <div class="border border-gray-900 rounded mb-4">
                         <textarea
                             class="w-full pt-4 pl-8 outline-none"
-                            placeholder="XXXXへのメッセージ"
+                            :placeholder="placeholder"
+                            v-model="message"
                         ></textarea>
                         <div class="bg-gray-100 p-2">
                             <button
                                 class="bg-green-900 text-sm text-white font-bold py-1 px-2 rounded"
+                                @click="sendMessage"
                             >
                                 送信
                             </button>
@@ -120,34 +169,15 @@ export default {
     data() {
         return {
             user: "",
-            users: [
-                {
-                    user_id: 11,
-                    email: "john@example.com",
-                },
-                {
-                    user_id: 12,
-                    email: "kevin@test.com",
-                },
-                {
-                    user_id: 13,
-                    email: "susan@test.com",
-                },
-            ],
-            channels: [
-                {
-                    id: 1,
-                    channel_name: "営業",
-                },
-                {
-                    id: 2,
-                    channel_name: "マーケティング",
-                },
-                {
-                    id: 3,
-                    channel_name: "情シス",
-                },
-            ],
+            users: [],
+            message: "",
+            messages: [],
+            placeholder: "",
+            channel_name: "",
+            channel_id: "",
+            channelModal: false,
+            channel: "",
+            channels: [],
         };
     },
     methods: {
@@ -155,9 +185,120 @@ export default {
             firebase.auth().signOut();
             this.$router.push("/signin");
         },
+        sendMessage() {
+            const newMessage = firebase
+                .database()
+                .ref("messages")
+                .child(this.channel_id)
+                .push();
+
+            const key_id = newMessage.key;
+
+            newMessage.set({
+                key: key_id,
+                content: this.message,
+                user: this.user.email,
+                createdAt: firebase.database.ServerValue.TIMESTAMP,
+            });
+
+            this.message = "";
+        },
+        directMessage(user) {
+            this.messages = [];
+            this.user.uid > user.user_id
+                ? (this.channel_id = this.user.uid + "-" + user.user_id)
+                : (this.channel_id = user.user_id + "-" + this.user.uid);
+
+            if (this.channel_id != "") {
+                firebase
+                    .database()
+                    .ref("messages")
+                    .child(this.channel_id)
+                    .off();
+            }
+
+            firebase
+                .database()
+                .ref("messages")
+                .child(this.channel_id)
+                .on("child_added", (snapshot) => {
+                    this.messages.push(snapshot.val());
+                });
+        },
+        showChannelModal() {
+            this.channelModal = true;
+        },
+        closeChannelModal() {
+            this.channelModal = false;
+        },
+        addChannel() {
+            const newChannel = firebase
+                .database()
+                .ref("channel")
+                .push();
+
+            const key_id = newChannel.key;
+
+            newChannel
+                .set({
+                    channel_name: this.channel,
+                    id: key_id,
+                })
+                .then(() => {
+                    this.channelModal = false;
+                });
+
+            this.channel = "";
+        },
+        channelMessage(channel) {
+            this.messages = [];
+            this.channel_name = "# " + channel.channel_name;
+            this.channel_id = channel.id;
+
+            if (this.channel_id != "") {
+                firebase
+                    .database()
+                    .ref("messages")
+                    .child(this.channel_id)
+                    .off();
+            }
+
+            firebase
+                .database()
+                .ref("messages")
+                .child(channel.id)
+                .on("child_added", (snapshot) => {
+                    this.messages.push(snapshot.val());
+                });
+        },
     },
     mounted() {
         this.user = firebase.auth().currentUser;
+        firebase
+            .database()
+            .ref("users")
+            .on("child_added", (snapshot) => {
+                this.users.push(snapshot.val());
+            });
+
+        firebase
+            .database()
+            .ref("channel")
+            .on("child_added", (snapshot) => {
+                this.channels.push(snapshot.val());
+            });
+    },
+    beforeDestroy() {
+        firebase
+            .database()
+            .ref("users")
+            .off();
+
+        firebase
+            .database()
+            .ref("messages")
+            .child(this.channel_id)
+            .off();
     },
 };
 </script>
